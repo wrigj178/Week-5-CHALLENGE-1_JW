d701cd819faa04ae31913e9de45b4aea3a0f8277
wrigj178 <jjazzymane@live.com>
2014-03-25 10:11:33 -0400

    Initial Commit

---
+++ SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Jasmine Wright on 3/25/14.
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitPhysicsTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SpriteKitPhysicsTest
+//
+
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
